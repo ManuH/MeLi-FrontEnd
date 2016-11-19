4129717e010aad7654fb38a141e6e561cf94e9ec
manuh <hemmanuel44@gmail.com>
2016-11-19 00:04:52 -0300

    updates

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 	<script src="https://ui.mlstatic.com/chico/tiny/0.2.3/tiny.min.js"></script>
 	<script src="dist/ui/chico.min.js"></script>
 	<link rel="stylesheet" href="style.css" />
+	<script src="javascript.js"></script>
 </head>
 <body>
 	<div class= "outter">
@@ -48,10 +49,45 @@
 					<input type="button" class="ch-btn" value="Button">
 					<dd>MercadoLibre isn't the owner of this product</dd>
 				</div>
-				<div class = "rightchild" > VIER </div>
+				<div class="demo-carousel ch-carousel">
+					<ul>
+						<li><img src="ipad1.jpg"></li>
+						<li><img src="ipad2.jpg"></li>
+						<li><img src="ipad3.jpg"></li>
+						<li><img src="ipad4.jpg"></li>
+						<script src="javascript.js"></script>
+						<script src="chico/dist/ui/chico.min.js"></script>
+					</ul>
+				</div>
 			</div>
 		</div>	
-		<div class= "inner" id= "bottom" > DESCRIPTION AND COMMENTS </div>	
-	</div>	
+		<div class= "inner" id= "bottom" >  
+			<div class="demo-tabs ch-tabs">
+				<ul class="ch-tabs-triggers">
+					<li><a href="#description" class="ch-tab">Product description</a></li>
+					<li><a href="#questions" class="ch-tab">Questions</a></li>
+					<link rel="stylesheet" href="sass.css" />
+				</ul>
+				<div class="ch-box-lite">
+					<div id="description">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id suscipit arcu, eget tristique elit. Nulla facilisi. Mauris sollicitudin arcu id blandit consequat. Donec ullamcorper tellus massa, feugiat consectetur metus efficitur et. Curabitur faucibus dui ut est venenatis tristique. Cras eu tortor accumsan, dapibus erat vitae, accumsan libero. Nulla ultrices commodo nisi, id gravida diam scelerisque eu. Aenean nec eleifend ligula. Fusce molestie lectus ante, nec vulputate orci ullamcorper in. Fusce ante tortor, aliquet sed ipsum et, facilisis ornare sem. Nulla facilisi. Suspendisse potenti. Vestibulum nec nunc urna. Donec in sapien sed arcu semper dignissim. Ut ornare purus quis diam maximus, ut feugiat metus condimentum. Donec molestie bibendum lacinia. Integer augue ex, vehicula ut dapibus ut, tincidunt ut est. Ut euismod purus nec tellus volutpat, non porta ligula finibus. Sed nec malesuada dui. Nam at ultricies libero, non efficitur lorem. Sed sed nunc sem. Mauris in condimentum dui. Aenean mi ex, lobortis sed egestas et, tristique a quam. Nulla sit amet tellus sit amet turpis scelerisque luctus sit amet non erat. Proin id orci ex. Vestibulum porttitor erat lectus, in dapibus elit semper at. Proin egestas interdum tincidunt. Donec at nisi ligula. Integer consequat arcu ac luctus vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas vestibulum erat vehicula arcu tristique blandit. Etiam elementum diam est, ut sagittis tortor pretium ac. Curabitur et gravida arcu, eget dapibus felis. Sed ornare ex blandit tellus commodo, in tincidunt lacus blandit. Nulla gravida volutpat tortor at convallis. Sed viverra purus porttitor quam laoreet, ac pharetra enim placerat. Vestibulum sed ipsum et massa accumsan auctor ac nec erat. Praesent vehicula mi a tempus facilisis. Aliquam ac libero vitae tortor dictum imperdiet. In sed dolor sapien. Suspendisse aliquam massa lacus, at consectetur elit porta a. Vestibulum in odio semper, rhoncus nunc a, congue odio. Maecenas non hendrerit ligula. Aenean erat erat, mattis nec velit a, lobortis bibendum augue. Quisque tristique vel diam sit amet tristique. Morbi non vehicula felis. Nullam vitae risus quis orci ullamcorper faucibus non vel metus. In et purus mi. In hac habitasse platea dictumst. Aliquam erat volutpat. Sed accumsan posuere magna venenatis tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc lobortis purus ut viverra aliquam. Aenean ultrices eros luctus, mattis libero sit amet, posuere velit.</p>
+					</div>
+					<div id="questions">
+					<p style="color: #0066FF">Buyer</p>
+						<div class="ch-box-info">
+							
+							<p>Do you have this one in white?</p>
+						</div>
+						<p style="color: green">Seller</p>
+						<div class="ch-box-ok">
+							
+							<p>No I don't, I only have black</p>
+						</div>
+					</div>	
+					<script src="chico/dist/ui/chico.min.js"></script>
+					<script src="javascript.js"></script>
+				</div>		
+			</div>			
+		</div>	
 </body>
 </html>
--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
-    pagination: false
+    pagination: true
 });
 var tabs = new ch.Tabs(ch(".demo-tabs")[0]);
