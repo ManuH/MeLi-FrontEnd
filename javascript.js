797d8f1784c9ea48b92b49ec422aa79f1df18d70
manuh <hemmanuel44@gmail.com>
2016-11-20 12:49:16 -0300

    fixed javascript

--- javascript.js
+++ javascript.js
@@ -2,3 +2,15 @@ var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
     pagination: false
 });
 var tabs = new ch.Tabs(ch(".demo-tabs")[0]);
+
+function makeOptions (id) {
+    var select, i, option;
+
+    select = document.getElementById( id );
+
+    for ( i = 1; i <= 10; i += 1 ) {
+        option = document.createElement( 'option' );
+        option.value = option.text = i;
+        select.add( option );
+    };
+}
